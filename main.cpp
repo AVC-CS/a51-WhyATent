b23d86933feb78621f26d0a00f09633b65d2bf40
WhyATent <WWyatnt@gmail.com>
2026-03-27 09:43:46 -0700

    a51 Done.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 // Complete the following program.
 #include <iostream>
 #include <iomanip>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    // TODO: declare variable n for the exponent input
-
-    // TODO: read n from standard input
-
-    // TODO: compute 2 to the power of n using a loop (no cmath pow)
-    // For positive n: multiply result by 2, n times
-    // For negative n: divide result by 2, |n| times
-
+    double result = 1;
+  int power;
+  cout << "Enter the power number: ";
+  cin >> power;
+  if (power>=0){
+    for (int i=0; i<=power; i++){
+      cout << fixed << setprecision(5) << result << endl;
+      result *= 2.0;
+    }
+  }
+  else if (power<0){
+    for (int i=0; i<=abs(power); i++){
+      cout << fixed << setprecision(5) << result << endl;
+      result /= (double)2;
+    }
+  }
 }
